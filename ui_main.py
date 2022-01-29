899b34e13f5c79a7ebcde66b0110ee9997bc956f
陈禹男 <cyn0407@live.com>
2022-01-29 17:56:58 +0800

    Update ui_main.py

--- ui_main.py
+++ ui_main.py
@@ -1,4 +1,7 @@
+import asyncio
 import threading, time, os, json, sys
+from copy import deepcopy
+
 from PySide6.QtCore import QObject, QThread, Signal, Slot, QFile
 from PySide6.QtWidgets import QWidgetItem, QListWidget, QMainWindow, QApplication, QPushButton, QVBoxLayout, QWidget, \
     QLabel, QWidgetItem, QListWidgetItem, QComboBox
@@ -15,6 +18,8 @@ spacer = ":"
 class MainForm(Ui_Widget):
     room_id = 737562
     room = live.LiveDanmaku(room_id)
+    start_button_pressed = False
+
     def __init__(self):
         super(Ui_Widget, self).__init__()
         self.ui = Ui_Widget()
@@ -41,23 +46,49 @@ class MainForm(Ui_Widget):
         room_id_start_index = room_id_text.find(":") + 1
         room_id = int(room_id_text[room_id_start_index:])
         print(room_id)
+        self.room_id = room_id
 
     def button_updatePort(self):
         print("implement me ")
 
     def button_StartButton(self):
-        room = live.LiveDanmaku(self.room_id)
-        sync(room.connect())
         print("start button")
+        self.start_button_pressed = True
+        # self.room = live.LiveDanmaku(self.room_id)
+        # loop = asyncio.new_event_loop()
+        # asyncio.set_event_loop(loop)
+        # tasks = [self.room.connect()]
+        # loop.run_until_complete(asyncio.wait(tasks))
+        my_room_thread = threading.Thread(target=self.room_thread)
+        my_room_thread.daemon = True
+        my_room_thread.start()
+        print("end of function")
+        print(self.start_button_pressed)
 
+    def room_thread(self):
+        self.room = live.LiveDanmaku(self.room_id)
+        sync(self.room.connect())
 
 
 @MainForm.room.on('ALL')
-async def All_msg(event):
+async def All_msg(self, event):
     print(event)
 
-if __name__ == '__main__':
+
+async def qt_ui():
     app = QApplication(sys.argv)
     window = MainForm()
     window.show()
     sys.exit(app.exec())
+
+
+
+if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    tasks = [qt_ui()]
+    loop.run_until_complete(asyncio.wait(tasks))
+    # app = QApplication(sys.argv)
+    # window = MainForm()
+    # window.show()
+    # sys.exit(app.exec())
